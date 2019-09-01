d5a87c4671ba7beea96b80467b68c22365cc1eeb
Sibin Rasiya (UST <U73115@ust-global.com>
2019-09-01 13:57:55 +0530

    Initial commit

---
+++ springboot-rest/src/main/java/com/knowledgefactory/knowledgefactorydemo/UserVo.java
@@ -0,0 +1,21 @@
+package com.knowledgefactory.knowledgefactorydemo;
+
+public class UserVo {
+
+	private String name;
+	private String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
