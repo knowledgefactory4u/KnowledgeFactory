a5f8f0c6dd0ac88b01d5ee8db57ac6336f71492a
Sibin Rasiya (UST <U73115@ust-global.com>
2019-09-12 16:40:15 +0530

    removed un used annotaton

--- springboot-hystrix/spring-rest-hystrix-consumer/src/main/java/com/knowledgefactory/knowledgefactorydemo/Controller.java
+++ springboot-hystrix/spring-rest-hystrix-consumer/src/main/java/com/knowledgefactory/knowledgefactorydemo/Controller.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableCircuitBreaker
+
 public class Controller {
 
 	@Autowired
