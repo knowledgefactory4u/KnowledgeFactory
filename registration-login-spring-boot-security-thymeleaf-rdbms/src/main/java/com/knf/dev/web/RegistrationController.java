97e4e3d8b6865aab6e98a74a6b0d2e3afc4587b9
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2020-12-20 15:02:53 +0530

    Add files via upload
    init

---
+++ registration-login-spring-boot-security-thymeleaf-rdbms/src/main/java/com/knf/dev/web/RegistrationController.java
@@ -0,0 +1,38 @@
+package com.knf.dev.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.knf.dev.dto.UserRegistrationDto;
+import com.knf.dev.service.UserService;
+
+@Controller
+@RequestMapping("/registration")
+public class RegistrationController {
+
+	private UserService userService;
+
+	public RegistrationController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+
+	@ModelAttribute("user")
+	public UserRegistrationDto userRegistrationDto() {
+		return new UserRegistrationDto();
+	}
+
+	@GetMapping
+	public String showRegistrationForm() {
+		return "registration";
+	}
+
+	@PostMapping
+	public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
+		userService.save(registrationDto);
+		return "redirect:/registration?success";
+	}
+}
---
+++ registration-login-spring-boot-security-thymeleaf-rdbms/src/test/java/net/javaguides/springboot/RegistrationLoginSpringBootSecurityThymeleafApplicationTests.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationLoginSpringBootSecurityThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
