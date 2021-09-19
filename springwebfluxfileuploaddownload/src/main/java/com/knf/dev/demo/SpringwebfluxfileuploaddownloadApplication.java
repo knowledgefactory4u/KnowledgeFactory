87c07b02e39eadaf76aa79199a3c0aa9a77fd040
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-09-19 22:45:28 +0530

    Add files via upload
    init

---
+++ springwebfluxfileuploaddownload/src/main/java/com/knf/dev/demo/SpringwebfluxfileuploaddownloadApplication.java
@@ -0,0 +1,13 @@
+package com.knf.dev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxfileuploaddownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxfileuploaddownloadApplication.class, args);
+	}
+
+}
