b161d617693d4364f7690cf4451a2773b5af7e4f
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-09-23 01:20:11 +0530

    Add files via upload
    init

---
+++ springoauth2-github/src/main/java/com/knf/dev/demo/springoauth2github/Springoauth2GithubApplication.java
@@ -0,0 +1,13 @@
+package com.knf.dev.demo.springoauth2github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springoauth2GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springoauth2GithubApplication.class, args);
+	}
+
+}
---
+++ springoauth2-github/src/main/java/com/knf/dev/demo/springoauth2github/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.knf.dev.demo.springoauth2github.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.
+        configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.
+        configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable().antMatcher("/**").authorizeRequests()
+                .antMatchers("/","/index").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login().permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutSuccessUrl("/");
+    }
+}
