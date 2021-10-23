b07a383c3389681850f2efe6eb3a28fcbe427eb7
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-10-23 23:12:36 +0530

    Delete SpringExportPDF directory

--- SpringExportPDF/src/main/java/com/knf/dev/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.knf.dev.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.knf.dev.model.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-}
