8024683a1529046de23e22a67d2613c6e5efa7a0
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-11-20 23:42:57 +0530

    Delete springboot_webflux_reactivemonogo directory

--- springboot_webflux_reactivemonogo/src/main/java/com/knf/reactivestack/dev/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knf.reactivestack.dev.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.knf.reactivestack.dev.domain.User;
-
-public interface UserRepository extends ReactiveMongoRepository<User, String> {
-    
-}
