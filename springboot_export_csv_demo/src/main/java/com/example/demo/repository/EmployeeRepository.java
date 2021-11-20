5727f5254303c723f12addbe592c4177cc735f5a
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-11-20 23:44:07 +0530

    Delete springboot_export_csv_demo directory

--- springboot_export_csv_demo/src/main/java/com/example/demo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.model.Employee;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-
-}
