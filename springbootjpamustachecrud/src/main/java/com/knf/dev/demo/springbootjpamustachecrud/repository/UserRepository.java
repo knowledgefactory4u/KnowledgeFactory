1a14d5e185899277a8e6355b57f7764e53baf9dc
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-11-20 23:46:24 +0530

    Delete springbootjpamustachecrud directory

--- springbootjpamustachecrud/src/main/java/com/knf/dev/demo/springbootjpamustachecrud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knf.dev.demo.springbootjpamustachecrud.repository;
-
-import com.knf.dev.demo.springbootjpamustachecrud.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
