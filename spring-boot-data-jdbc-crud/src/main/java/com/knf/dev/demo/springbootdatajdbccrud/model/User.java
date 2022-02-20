8bcf12ab7ac3680b019b3e7961a615b6fb910d1c
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2022-02-21 00:04:54 +0530

    Update User.java

--- spring-boot-data-jdbc-crud/src/main/java/com/knf/dev/demo/springbootdatajdbccrud/model/User.java
+++ spring-boot-data-jdbc-crud/src/main/java/com/knf/dev/demo/springbootdatajdbccrud/model/User.java
@@ -1,42 +1,8 @@
 package com.knf.dev.demo.springbootdatajdbccrud.model;
 
-public class User {
-
-	private Long id;
-	private String firstName;
-	private String lastName;
-	private String email;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
+public record User
+(Long id, 
+    String firstName, 
+        String lastName,
+          String email) {
 }
