b3c69c457e964e3978234c2b9b2ed081a11eebcc
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2022-08-10 12:48:10 +0530

    Add files via upload

---
+++ springjpagroovycrud/src/test/java/com/knf/dev/demo/springjpagroovycrud/SpringjpagroovycrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knf.dev.demo.springjpagroovycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjpagroovycrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
