293ad0c9f2ef9401111f2c8450cc48767316eb47
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2022-08-10 12:50:41 +0530

    Add files via upload

---
+++ springbootfreemarkerjpacrud/src/main/java/com/knf/dev/demo/springbootfreemarkerjpacrud/SpringbootfreemarkerjpacrudApplication.java
@@ -0,0 +1,13 @@
+package com.knf.dev.demo.springbootfreemarkerjpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfreemarkerjpacrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfreemarkerjpacrudApplication.class, args);
+	}
+
+}
