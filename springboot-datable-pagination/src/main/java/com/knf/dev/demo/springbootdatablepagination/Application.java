55e06a5145e0269f745e42af4d7c2172c0b561d1
knowledgefactory4u <knowledgefactory4upeoples@gmail.com>
2022-08-11 18:39:18 +0530

    java version

--- springboot-datable-pagination/src/main/java/com/knf/dev/demo/springbootdatablepagination/Application.java
+++ springboot-datable-pagination/src/main/java/com/knf/dev/demo/springbootdatablepagination/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootdatablepaginationApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdatablepaginationApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
