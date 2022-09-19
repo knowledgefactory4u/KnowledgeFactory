aa3dac5fbac015dc70ae37a3d1c5a9cc6753cee9
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2022-09-19 13:09:58 +0530

    Update GlobalExceptionHandler.java

--- springboot-azure/spring-jdbc-azure-mysql-crud/src/main/java/com/knf/dev/demo/exception/GlobalExceptionHandler.java
+++ springboot-azure/spring-jdbc-azure-mysql-crud/src/main/java/com/knf/dev/demo/exception/GlobalExceptionHandler.java
@@ -4,24 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
-    public ResponseEntity<ErrorMessage> 
-       internalServerError(Exception ex, WebRequest request) {
-        var errors = 
-           new ErrorMessage(500, new Date(),
-              ex.getMessage(), "Internal Server Error");
-
-        return new ResponseEntity<>
-            (errors, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
 
     @ExceptionHandler(UserNotFound.class)
     public ResponseEntity<ErrorMessage> 
