b7c2b48a33319595eb6d5ba9f020b428fb09d8e3
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-01-19 17:18:23 +0530

    Add files via upload

---
+++ spring-get-post-put-delete-mapping-example/src/main/java/com/knf/dev/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.knf.dev.demo.exception;
+
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
