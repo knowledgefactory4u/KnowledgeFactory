834deab84b71722f805e09608c909cc8f8ecea48
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-02-14 00:59:19 +0530

    Update schema.sql

--- spring-boot-postgresql-mybatis-crud/src/main/resources/schema.sql
+++ spring-boot-postgresql-mybatis-crud/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users
 (
    id SERIAL PRIMARY KEY,
-   firstName varchar(255) not null,
-   lastName varchar(255) not null,
-   emailId varchar(255) not null
+   first_name varchar(255) not null,
+   last_name varchar(255) not null,
+   email_id varchar(255) not null
 );
