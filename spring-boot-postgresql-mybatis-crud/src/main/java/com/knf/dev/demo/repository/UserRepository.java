ea4bbce6ffc8ee2fb4513a413b46718dd987d997
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-02-14 00:59:52 +0530

    Update UserRepository.java

--- spring-boot-postgresql-mybatis-crud/src/main/java/com/knf/dev/demo/repository/UserRepository.java
+++ spring-boot-postgresql-mybatis-crud/src/main/java/com/knf/dev/demo/repository/UserRepository.java
@@ -9,19 +9,29 @@ import java.util.Optional;
 public interface UserRepository {
 
     @Select("select * from users")
-    public List<User> findAll();
+    @Results({
+            @Result(property = "firstName", column = "first_name"),
+            @Result(property = "lastName", column = "last_name"),
+            @Result(property = "emailId", column = "email_id")
+    })
+     List<User> findAll();
 
     @Select("SELECT * FROM users WHERE id = #{id}")
-    public Optional<User> findById(Integer id);
+    @Results({
+            @Result(property = "firstName", column = "first_name"),
+            @Result(property = "lastName", column = "last_name"),
+            @Result(property = "emailId", column = "email_id")
+    })
+     Optional<User> findById(Integer id);
 
     @Delete("DELETE FROM users WHERE id = #{id}")
-    public int deleteById(Integer id);
+     int deleteById(Integer id);
 
-    @Insert("INSERT INTO users(firstName, lastName,emailId) " +
+    @Insert("INSERT INTO users(first_name, last_name, email_id) " +
          " VALUES (#{firstName}, #{lastName}, #{emailId})")
-    public int insert(User user);
+     int insert(User user);
 
-    @Update("Update users set firstName=#{firstName}, " +
-         " lastName=#{lastName}, emailId=#{emailId} where id=#{id}")
-    public int update(User user);
+    @Update("Update users set first_name=#{firstName}, " +
+         " last_name=#{lastName}, email_id=#{emailId} where id=#{id}")
+     int update(User user);
 }
