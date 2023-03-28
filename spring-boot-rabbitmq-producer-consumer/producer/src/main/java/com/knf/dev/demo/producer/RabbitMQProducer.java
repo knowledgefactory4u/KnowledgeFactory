cf45acb24258440e70590e5b803834dbd74f9765
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-28 19:58:43 +0530

    Add files via upload

---
+++ spring-boot-rabbitmq-producer-consumer/producer/src/main/java/com/knf/dev/demo/producer/RabbitMQProducer.java
@@ -0,0 +1,27 @@
+package com.knf.dev.demo.producer;
+
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+
+    public void sendMessage(String message){
+
+        rabbitTemplate.convertAndSend(
+                exchange, routingKey, message);
+    }
+}
