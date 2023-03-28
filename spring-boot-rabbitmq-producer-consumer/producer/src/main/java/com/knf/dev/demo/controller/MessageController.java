7bdfa43c9687e0f4ef4b98e93da36fdd041d5a11
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-28 15:12:59 +0530

    Add files via upload

---
+++ spring-boot-rabbitmq-producer-consumer/producer/src/main/java/com/knf/dev/demo/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.knf.dev.demo.controller;
+
+import com.knf.dev.demo.producer.RabbitMQProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @Autowired
+    private RabbitMQProducer rabbitMQProducer;
+
+    @PostMapping("/postMessage")
+    public String publishMessage(@RequestParam("message") String message) {
+
+        rabbitMQProducer.sendMessage(message);
+        return "Message was sent successfully";
+    }
+}
