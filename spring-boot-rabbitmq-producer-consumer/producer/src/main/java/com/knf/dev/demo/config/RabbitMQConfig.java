399290185eb8f6a450fc973f35645c23531677de
Sibin Rasiya <54763130+knowledgefactory4u@users.noreply.github.com>
2023-03-28 14:51:09 +0530

    Add files via upload

---
+++ spring-boot-rabbitmq-producer-consumer/producer/src/main/java/com/knf/dev/demo/config/RabbitMQConfig.java
@@ -0,0 +1,43 @@
+package com.knf.dev.demo.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    Queue createQueue() {
+        return new Queue(queue);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+}
